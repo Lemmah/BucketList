bd2f90f24e75be470b0208e12228d64803c2c2ea
Lemmah <lemayiannakolah@gmail.com>
2017-07-18 23:05:03 +0300

    [update] test update bucketlist item details
    This code creates a new instance of a bucketlist item using the universal details.
    Then Manipulates the instance to update the details of the just created bucketlist item
    It changes the name.

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -3,6 +3,7 @@ import unittest
 from app.bucketlist import BucketList
 from app.bucketlist_item import BucketListItem
 
+
 class TestBucketList(unittest.TestCase):
     ''' Testing or defining the bucketlist '''
 
@@ -11,7 +12,7 @@ class TestBucketList(unittest.TestCase):
         self.new_bucketlist = BucketList("Test Bucket")
         self.new_item_details = ("ItemName", "Category", "Description: optional")
 
-    #1. Tests for successful creating of a new_bucketlist instance
+    # 1. Tests for successful creating of a new_bucketlist instance
     def test_bucketlist_is_created(self):
         ''' Checking that bucketlist has been created '''
         assertTrue(isinstance(self.new_bucketlist, BucketList))
@@ -28,7 +29,7 @@ class TestBucketList(unittest.TestCase):
         ''' Asserting that each bucketlist has an owner '''
         assertNotTrue(isinstance(self.new_bucketlist.owner, None))
 
-    #2. Tests for bucketlist behaviors
+    # 2. Tests for bucketlist behaviors
     def test_add_bucketlist_item(self):
         ''' Testing adding items to a bucketlist '''
         # see that an item has been added successfully by message return
@@ -39,11 +40,22 @@ class TestBucketList(unittest.TestCase):
         # check and ascertain that a bucketlist_item object is created
         assertTrue(isinstance(new_item[0], BucketListItem))
 
-
     def test_update_bucketlist_item_details(self):
         ''' Testing for updating bucketlist item details '''
         # This depends on successful creation of a new item
-        pass
+        new_item = self.new_bucketlist.add_item(self.new_item_details)
+        # The new item object is returned by the add_item method in a tuple at the first index
+        # So,
+        new_item = new_item[0]  # ommitting the success message
+        # First check that the new item is indeed a BucketListItem
+        assertTrue(isinstance(new_item, BucketListItem))
+        # unpark the details
+        item_name, category, description = new_item.name, new_item.category, new_item.description
+        # confirming that name is as in new_item_details
+        assertEqual(item_name, self.new_item_detais[0])
+        # Test for changing the name
+        item_name = "ChangedName"
+        assertEqual(new_item.name, "ChangedName")
 
     def test_remove_bucketlist_item(self):
         ''' Testing that items are removed from the list '''
@@ -52,5 +64,3 @@ class TestBucketList(unittest.TestCase):
         assertEqual(remove_item, "ItemName removed successfully")
         # ensure that the item is no longer in the list
         assertNotIn(self.new_item_name, self.new_bucketlist.items)
-
-
