12521da2cfb01d91aa218c2d7b71122f4eb56bb0
Lemmah <lemayiannakolah@gmail.com>
2017-07-18 22:47:12 +0300

    [update] redesign handling of bucketlist items
    BucketListItem is supposed to be instantiated when the add_item method is called and then returned in a tuple alongside a success message.

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -9,12 +9,12 @@ class TestBucketList(unittest.TestCase):
     def setUp(self):
         ''' Instatiating reusable variables '''
         self.new_bucketlist = BucketList("Test Bucket")
-        self.new_item_name = "ItemName"
+        self.new_item_details = ("ItemName", "Category", "Description: optional")
 
     #1. Tests for successful creating of a new_bucketlist instance
     def test_bucketlist_is_created(self):
         ''' Checking that bucketlist has been created '''
-        assertTrue(isinstance(self.new_bucketlist, "BucketList"))
+        assertTrue(isinstance(self.new_bucketlist, BucketList))
 
     def test_bucketlist_has_id(self):
         ''' Asserting that any bucketlist created has an id '''
@@ -32,14 +32,17 @@ class TestBucketList(unittest.TestCase):
     def test_add_bucketlist_item(self):
         ''' Testing adding items to a bucketlist '''
         # see that an item has been added successfully by message return
-        add_item = self.new_bucketlist.add_item(self.new_item_name, "Category", "Description: optional")
-        assertEqual(add_item, "ItemName added successfully")
+        new_item = self.new_bucketlist.add_item(self.new_item_details)
+        assertEqual(add_item[1], "ItemName added successfully")
         # ensure that it is indeed in the list of items
-        assertIn(self.new_item_name, self.new_bucketlist.items)
+        assertIn(new_item.name, self.new_bucketlist.items)
+        # check and ascertain that a bucketlist_item object is created
+        assertTrue(isinstance(new_item[0], BucketListItem))
+
 
     def test_update_bucketlist_item_details(self):
         ''' Testing for updating bucketlist item details '''
-        # This will require me to implement the bucketlist item object first
+        # This depends on successful creation of a new item
         pass
 
     def test_remove_bucketlist_item(self):
