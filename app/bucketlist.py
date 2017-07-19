c2b3296c6af0e2c273f73a78b4c4297f984d9c72
Lemmah <lemayiannakolah@gmail.com>
2017-07-20 00:01:07 +0300

    [feature] construct bucketlist object

--- app/bucketlist.py
+++ app/bucketlist.py
@@ -1,5 +1,20 @@
-# This is the structure of the bucketlist
+#  This is the structure of the bucketlist
+
+from app.bucketlist_item import BucketListItem
 
 
 class BucketList:
-    pass
+    ''' The bucketlist object '''
+
+    def __init__(self, name, details=None, items=None, owner=None):
+        ''' Constructing and instance of the object '''
+        # Every bucketlist must have an owner
+        if owner is None:
+            raise Exception("Every bucketlist must have an owner")
+        self.name = name
+        self.details = details
+        if items is not None:
+            self.items = items
+        else:
+            self.items = []
+        self.owner = owner
