73c6ee8de0a75284c732403e84aad446c253cdec
Lemmah <lemayiannakolah@gmail.com>
2017-07-19 09:04:52 +0300

    [update] setup for testing bucketlist items

--- tests/test_bucketlist_item.py
+++ tests/test_bucketlist_item.py
@@ -3,6 +3,11 @@
 import unittest
 from app.bucketlist_item import BucketListItem
 
+
 class TestBucketListItem(unittest.TestCase):
-    ''' Testing the bucketlist object '''
-    pass
+    ''' Testing the bucketlist item object '''
+
+    def setUp(self):
+        ''' Initializing reusable variables '''
+        self.bucketlist_item_details = ("bucketlist_item_name", "category", "description: optional", "bucketlist_id")
+        self.bucketlist_item = BucketListItem(self.bucketlist_item_details)
