5ecd5090c3fdf5837808eeb62687a1762aff6301
Lemmah <lemayiannakolah@gmail.com>
2017-07-19 11:43:47 +0300

    [fix] idendation error

--- tests/test_bucketlist_item.py
+++ tests/test_bucketlist_item.py
@@ -12,35 +12,35 @@ class TestBucketListItem(unittest.TestCase):
         self.bucketlist_item_details = ("bucketlist_item_name", "category", "description: optional", "bucketlist_id")
         self.bucketlist_item = BucketListItem(self.bucketlist_item_details)
 
-        # 1. Ensure that a bucketlist item is created
-        def test_bucketlist_item_created(self):
-            ''' Ensuring that a bucketlist item has been instantiated '''
-            assertTrue(isinstance(self.bucketlist_item, BucketListItem))
+    # 1. Ensure that a bucketlist item is created
+    def test_bucketlist_item_created(self):
+        ''' Ensuring that a bucketlist item has been instantiated '''
+        assertTrue(isinstance(self.bucketlist_item, BucketListItem))
 
-        def test_bucketlist_item_belongs_to_bucketlist(self):
-            ''' Ensuring that the bucketlist_id is not None '''
-            assertNotTrue(isinstance(self.bucketlist_item.bucketlist_id, None))
+    def test_bucketlist_item_belongs_to_bucketlist(self):
+        ''' Ensuring that the bucketlist_id is not None '''
+        assertNotTrue(isinstance(self.bucketlist_item.bucketlist_id, None))
 
-        def test_bucketlist_item_has_status(self):
-            ''' Assert that bucketlist_item is either done or in_progress '''
-            assertNotTrue(isinstance(self.bucketlist_item.status, None))
+    def test_bucketlist_item_has_status(self):
+        ''' Assert that bucketlist_item is either done or in_progress '''
+        assertNotTrue(isinstance(self.bucketlist_item.status, None))
 
-        # 2. Test bucketlist item behaviours
-        def test_bucketlist_item_change_status(self):
-            ''' Test that status of bucketList can change from done to in_progress and viceversa '''
+    # 2. Test bucketlist item behaviours
+    def test_bucketlist_item_change_status(self):
+        ''' Test that status of bucketList can change from done to in_progress and viceversa '''
+        bucketlist_item_status = self.bucketlist_item.status.lower()
+        if bucketlist_item_status == "done":
+            self.bucketlist_item.change_status()
             bucketlist_item_status = self.bucketlist_item.status.lower()
-            if bucketlist_item_status == "done":
-                self.bucketlist_item.change_status()
-                bucketlist_item_status = self.bucketlist_item.status.lower()
-                assertNotTrue(bucketlist_item_status == "done")
-            else:
-                self.bucketlist_item.change_status()
-                bucketlist_item_status = self.bucketlist_item.status.lower()
-                assertEqual(bucketlist_item_status, "done")
+            assertNotTrue(bucketlist_item_status == "done")
+        else:
+            self.bucketlist_item.change_status()
+            bucketlist_item_status = self.bucketlist_item.status.lower()
+            assertEqual(bucketlist_item_status, "done")
 
-        # 3. Handling edge cases
-        def test_bucketlist_item_duplicate_item(self):
-            ''' Test that duplicate bucketlist_items raise exception '''
-            # creating a bucketlist with existing details
-            duplicate_bucketlist_item = BucketListItem(self.bucketlist_item_details)
-            assertTrue(isinstance(duplicate_bucketlist_item, Exception))
+    # 3. Handling edge cases
+    def test_bucketlist_item_duplicate_item(self):
+        ''' Test that duplicate bucketlist_items raise exception '''
+        # creating a bucketlist with existing details
+        duplicate_bucketlist_item = BucketListItem(self.bucketlist_item_details)
+        assertTrue(isinstance(duplicate_bucketlist_item, Exception))
