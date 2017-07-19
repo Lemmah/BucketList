03b457e73ecbbe822839db03a1ba4a914bf8702b
Lemmah <lemayiannakolah@gmail.com>
2017-07-20 00:08:45 +0300

    [feature] enable bucketlist controller to add bucketlist

--- app/bucketlist_controller.py
+++ app/bucketlist_controller.py
@@ -10,3 +10,14 @@ class BucketListController:
         ''' Construct the bucketlist controller '''
         self.user = user
         self.available_bucketlists = []
+
+    def add_bucketlist(self, bucketlist_details):
+        ''' Functionality to create new bucketlist '''
+        owner = self.user
+        bucketlist_name = bucketlist_details[0]
+        if bucketlist_name in self.available_bucketlists:
+            raise Exception("A bucketlist with the name {} already exists")
+        # add name of new bucketlist to list of available bucketlists
+        self.available_bucketlists.append(bucketlist_details[0])
+        new_bucketlist = BucketList(*bucketlist_details, owner=self.user)
+        return (new_bucketlist, "{} bucketlist has been created".format(bucketlist_name))
