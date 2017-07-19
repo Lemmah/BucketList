1f1ba404e3009925053e48fb84d84124fd94e657
Lemmah <lemayiannakolah@gmail.com>
2017-07-19 13:17:23 +0300

    [fix #inheritance] subclass assertions to the TestCase class
    Adding self to all assertions to subclass them
    Removing assertions that are not in the testing suite

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -15,30 +15,30 @@ class TestBucketList(unittest.TestCase):
     # 1. Tests for successful creating of a new_bucketlist instance
     def test_bucketlist_is_created(self):
         ''' Checking that bucketlist has been created '''
-        assertTrue(isinstance(self.new_bucketlist, BucketList))
+        self.assertTrue(isinstance(self.new_bucketlist, BucketList))
 
     def test_bucketlist_has_id(self):
         ''' Asserting that any bucketlist created has an id '''
-        assertNotEqual(self.new_bucketlist.bucket_id, None)
+        self.assertNotEqual(self.new_bucketlist.bucket_id, None)
 
     def test_bucketlist_items_is_list(self):
         ''' Asserting that a bucketlist can take a list of items '''
-        assertTrue(isinstance(self.new_bucketlist.items, list))
+        self.assertTrue(isinstance(self.new_bucketlist.items, list))
 
     def test_bucketlist_has_owner(self):
         ''' Asserting that each bucketlist has an owner '''
-        assertNotTrue(self.new_bucketlist.owner, None)
+        self.assertNotEqual(self.new_bucketlist.owner, None)
 
     # 2. Tests for bucketlist behaviors
     def test_add_bucketlist_item(self):
         ''' Testing adding items to a bucketlist '''
         # see that an item has been added successfully by message return
         new_item = self.new_bucketlist.add_item(self.new_item_details)
-        assertEqual(add_item[1], "ItemName added successfully")
+        self.assertEqual(add_item[1], "ItemName added successfully")
         # ensure that it is indeed in the list of items
-        assertIn(new_item.name, self.new_bucketlist.items)
+        self.assertIn(new_item.name, self.new_bucketlist.items)
         # check and ascertain that a bucketlist_item object is created
-        assertTrue(isinstance(new_item[0], BucketListItem))
+        self.assertTrue(isinstance(new_item[0], BucketListItem))
 
     def test_update_bucketlist_item_details(self):
         ''' Testing for updating bucketlist item details '''
@@ -48,26 +48,26 @@ class TestBucketList(unittest.TestCase):
         # So,
         new_item = new_item[0]  # ommitting the success message
         # First check that the new item is indeed a BucketListItem
-        assertTrue(isinstance(new_item, BucketListItem))
+        self.assertTrue(isinstance(new_item, BucketListItem))
         # unpark the details
         item_name, category, description = new_item.name, new_item.category, new_item.description
         # confirming that name is as in new_item_details
-        assertEqual(item_name, self.new_item_details[0])
+        self.assertEqual(item_name, self.new_item_details[0])
         # Test for changing the all the item details
         item_name, category, description = "ChangedName", "ChangedCategory", "ChangedDescription"
-        assertEqual((new_item.name, new_item.category, new_item.details), ("ChangedName", "ChangedCategory", "ChangedDescription"))
+        self.assertEqual((new_item.name, new_item.category, new_item.details), ("ChangedName", "ChangedCategory", "ChangedDescription"))
 
     def test_remove_bucketlist_item(self):
         ''' Testing that items are removed from the list '''
         # Create item before removal
         new_item = self.new_bucketlist.add_item(self.new_item_details)
-        # assert message returned on removal
+        # self.assert message returned on removal
         remove_item = self.new_bucketlist.remove_item(self.new_item_details[0])
-        assertEqual(remove_item[1], "ItemName removed successfully")
+        self.assertEqual(remove_item[1], "ItemName removed successfully")
         # ensure that the item is no longer in the list
-        assertNotIn(self.new_item_details[0], self.new_bucketlist.items)
+        self.assertNotIn(self.new_item_details[0], self.new_bucketlist.items)
         # ensure that the item details are indeed deleted from the object instance
-        assertEqual(new_item.name, None)
+        self.assertEqual(new_item.name, None)
 
     # 3. Testing for handling of edge cases
     def test_adding_same_bucketlist_items_twice(self):
@@ -75,15 +75,15 @@ class TestBucketList(unittest.TestCase):
         # create a bucketlist item
         new_item = self.new_bucketlist.add_item(self.new_item_details)
         # ensure that calling the add_item method with the same details raises and exception
-        assertRaises(Exception, self.new_bucketlist.add_item, self.new_item_details)
+        self.assertRaises(Exception, self.new_bucketlist.add_item, self.new_item_details)
 
     def test_deleting_non_existing_bucketlist_item(self):
         ''' Testing that non-existing items do not get other items deleted '''
         # try removing item that does not exist
         remove_item = self.new_bucketlist.remove_item(self.new_item_details[0])
-        assertEqual(remove_item, "{} does not exist.".format(self.new_item_details[0]))
+        self.assertEqual(remove_item, "{} does not exist.".format(self.new_item_details[0]))
         # assert that we're raising exceptions
-        assertTrue(isinstance(remove_item, Exception))
+        self.assertTrue(isinstance(remove_item, Exception))
 
     def test_creating_duplicate_bucketlist(self):
         ''' Test raising exception when a bucket that already exists by name is created '''
@@ -91,6 +91,6 @@ class TestBucketList(unittest.TestCase):
         bucket_name = "Test Bucket"
         duplicate_bucket = BucketList(bucket_name)
         # raising custom exception
-        assertEqual(duplicate_bucket, "A bucketlist with the name {} already exists.".format(bucket_name))
+        self.assertEqual(duplicate_bucket, "A bucketlist with the name {} already exists.".format(bucket_name))
         # assert that it's indeed and exception
-        assertTrue(isinstance(duplicate_bucket, Exception))
+        self.assertTrue(isinstance(duplicate_bucket, Exception))
--- tests/test_bucketlist_item.py
+++ tests/test_bucketlist_item.py
@@ -9,21 +9,21 @@ class TestBucketListItem(unittest.TestCase):
 
     def setUp(self):
         ''' Initializing reusable variables '''
-        self.bucketlist_item_details = "bucketlist_item_name", "category", "description: optional", "bucketlist_id"
-        self.bucketlist_item = BucketListItem("bucketlist_item_name", "category", "description: optional", "bucketlist_id")
+        self.name, self.category, self.description, self.bucketlist = "bucketlist_item_name", "category", "description: optional", "bucketlist_id"
+        self.bucketlist_item = BucketListItem(self.name, self.category, self.description, self.bucketlist)
 
     # 1. Ensure that a bucketlist item is created
     def test_bucketlist_item_created(self):
         ''' Ensuring that a bucketlist item has been instantiated '''
-        assertEqual(isinstance(self.bucketlist_item, BucketListItem), True)
+        self.assertTrue(isinstance(self.bucketlist_item, BucketListItem))
 
     def test_bucketlist_item_belongs_to_bucketlist(self):
         ''' Ensuring that the bucketlist_id is not None '''
-        assertNotTrue(isinstance(self.bucketlist_item.bucketlist_id, None))
+        self.assertNotEqual(self.bucketlist_item.bucketlist_id, None)
 
     def test_bucketlist_item_has_status(self):
-        ''' Assert that bucketlist_item is either done or in_progress '''
-        assertNotTrue(isinstance(self.bucketlist_item.status, None))
+        ''' self.assert that bucketlist_item is either done or in_progress '''
+        self.assertNotEqual(self.bucketlist_item.status, None)
 
     # 2. Test bucketlist item behaviours
     def test_bucketlist_item_change_status(self):
@@ -32,15 +32,9 @@ class TestBucketListItem(unittest.TestCase):
         if bucketlist_item_status == "done":
             self.bucketlist_item.change_status()
             bucketlist_item_status = self.bucketlist_item.status.lower()
-            assertNotTrue(bucketlist_item_status == "done")
+            self.assertNotEqual(bucketlist_item_status == "done", True)
         else:
             self.bucketlist_item.change_status()
             bucketlist_item_status = self.bucketlist_item.status.lower()
-            assertEqual(bucketlist_item_status, "done")
+            self.assertEqual(bucketlist_item_status, "done")
 
-    # 3. Handling edge cases
-    def test_bucketlist_item_duplicate_item(self):
-        ''' Test that duplicate bucketlist_items raise exception '''
-        # creating a bucketlist with existing details
-        duplicate_bucketlist_item = BucketListItem(self.bucketlist_item_details)
-        assertTrue(isinstance(duplicate_bucketlist_item, Exception))
