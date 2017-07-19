bbe2c0c296ccd9e3d67841efe0bb5facc7981d62
Lemmah <lemayiannakolah@gmail.com>
2017-07-19 20:47:33 +0300

    [fix] refactor tests to refine functionality before implementation

--- tests/test_bucketlist_controller.py
+++ tests/test_bucketlist_controller.py
@@ -5,7 +5,7 @@ from app.bucketlist_controller import BucketListController
 from app.bucketlist import BucketList
 
 
-class TestBucketListController:
+class TestBucketListController(unittest.TestCase):
     ''' Testing the control bucketlist feature '''
     # This class is just a layer that enable the user to control their bucketlists
 
@@ -13,31 +13,38 @@ class TestBucketListController:
         ''' Initializing reusable variables '''
         self.test_user = "Test User"
         self.bucketlist_controller = BucketListController(self.test_user)
-        self.bucketlist_details = ("ItemName", "Category", "Description: optional")
-        self.bucketlist = BucketList(self.bucketlist_details, self.test_user)
+        self.bucketlist_details = ("ItemName", "Description: optional")
+        self.bucketlist = BucketList(*self.bucketlist_details, owner=self.test_user)
 
     # 1. Test successfull instantiation
     def test_bucketlist_controller_creation(self):
         ''' Asserting that a bucketlist controller can be created '''
-        assertTrue(isinstance(bucketlist_controller, BucketListController))
+        self.assertEqual(isinstance(self.bucketlist_controller, BucketListController), True)
 
     def test_bucketlist_controller_has_available_bucketlists(self):
         ''' Asserting that the bucketlist controller knows about available bucketlists '''
-        assertTrue(isinstance(BucketListController.available_bucketlists, list))
+        self.assertEqual(isinstance(self.bucketlist_controller.available_bucketlists, list), True)
 
     # 2. Test bucketlist controller operations
     def test_bucketlist_controller_can_create_bucketlist(self):
         ''' Asserting that a bucketlist controller can create a bucketlist '''
-        new_bucketlist_details = ("NewName", "NewCategory", "Description: Optional")
-        new_bucketlist = self.bucketlist_controller.add_bucketlist(new_bucketlist_details, self.test_user)
-        assertTrue(isinstance(new_bucketlist, BucketList))
+        new_bucketlist_details = ("NewName", "Description: Optional")
+        new_bucketlist = self.bucketlist_controller.add_bucketlist(new_bucketlist_details)
+        self.assertEqual(isinstance(new_bucketlist, BucketList), True)
 
     def test_bucketlist_controller_can_update_bucketlist(self):
         ''' Asserting that bucketlist controller can change bucketlist details '''
-        pass
+        # rename, change details
+        new_name, new_description = "NewBucket", "NewDetails: This is new"
+        rename = self.bucketlist_controller.rename_bucketlist(self.bucketlist.name, new_name)
+        self.assertEqual(rename, "{} bucketlist has been renamed to {}".format(self.bucketlist.name, new_name))
+        self.assertEqual(self.bucketlist.name, new_name)
+        change_details = self.bucketlist_controller.change_bucketlist_details(self.bucketlist, new_description)
+        self.assertEqual(change_details, "{} has been updated accordingly".format(self.bucketlist.name))
+        self.assertEqual(self.bucketlist.description, new_description)
 
     def test_bucketlist_controller_can_delete_bucketlist(self):
         ''' Asserting that a bucketlist controller can delete a bucketlist '''
         bucketlist_name = self.bucketlist_details[0]
         delete_bucketlist = self.bucketlist_controller.delete_bucketlist(bucketlist_name)
-        assertEqual(self.bucketlist, None)
+        self.assertEqual(self.bucketlist, None)
