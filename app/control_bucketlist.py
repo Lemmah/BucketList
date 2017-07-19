200372d4c3c8381dd3fbef61251616d7cc326782
Lemmah <lemayiannakolah@gmail.com>
2017-07-19 19:58:18 +0300

    [fix] removed control_bucketlist file, not usable

--- app/control_bucketlist.py
+++
@@ -1,5 +0,0 @@
-# This is the super class, Controlling all the operations on the bucketlist and bucketlist items
-
-class ControlBucketList:
-    ''' Perfoming CRUD operations on bucket lists '''
-    pass
