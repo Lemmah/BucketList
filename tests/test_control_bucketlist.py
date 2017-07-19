69211ae10e877d48bb2a3db2129cd21446c4ea27
Lemmah <lemayiannakolah@gmail.com>
2017-07-19 09:29:36 +0300

    [new] Correctly name BucketListController Object
    An object cannot be named like an action so renamed test_contol_bucketlist.py to test_bucketlist_controller.py

--- tests/test_control_bucketlist.py
+++
@@ -1,8 +0,0 @@
-# Testing the super class that performs CRUD operations
-
-import unittest
-from app.control_bucketlist import ControlBucketList
-
-class TestControlBucketList:
-    ''' Testing the control bucketlist feature '''
-    pass
