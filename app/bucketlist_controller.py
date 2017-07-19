202ef84db35b97183a8374f1a0d31af64915023e
Lemmah <lemayiannakolah@gmail.com>
2017-07-19 11:28:49 +0300

    [update] rename control bucketlist to bucketlist controller

---
+++ app/bucketlist_controller.py
@@ -0,0 +1,5 @@
+# This is the super class, Controlling all the operations on the bucketlist and bucketlist items
+
+class BucketListController:
+    ''' Perfoming CRUD operations on bucket lists '''
+    pass
