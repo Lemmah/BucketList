de1fe4cc544a398e35b34ab5ddaae20a11a41cb8
Lemmah <lemayiannakolah@gmail.com>
2017-07-19 09:07:15 +0300

    [update] test bucketlist item creation
    Ensure that a bucketlist item is created of type BucketListItem and has a status an owner bucketlist

--- tests/test_bucketlist_item.py
+++ tests/test_bucketlist_item.py
@@ -11,3 +11,16 @@ class TestBucketListItem(unittest.TestCase):
         ''' Initializing reusable variables '''
         self.bucketlist_item_details = ("bucketlist_item_name", "category", "description: optional", "bucketlist_id")
         self.bucketlist_item = BucketListItem(self.bucketlist_item_details)
+
+        # 1. Ensure that a bucketlist item is created
+        def test_bucketlist_item_created(self):
+            ''' Ensuring that a bucketlist item has been instantiated '''
+            assertTrue(isinstance(self.bucketlist_item, BucketListItem))
+
+        def test_bucketlist_item_belongs_to_bucketlist(self):
+            ''' Ensuring that the bucketlist_id is not None '''
+            assertNotTrue(isinstance(self.bucketlist_item.bucketlist_id, None))
+
+        def test_bucketlist_item_has_status(self):
+            ''' Assert that bucketlist_item is either done or in_progress '''
+            assertNotTrue(isinstance(self.bucketlist_item.status, None))
