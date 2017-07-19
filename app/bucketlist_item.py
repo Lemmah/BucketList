3275e49f81c5d19805a81065560199434a96ccb5
Lemmah <lemayiannakolah@gmail.com>
2017-07-19 23:58:56 +0300

    [feature] construct the bucketlist item object

--- app/bucketlist_item.py
+++ app/bucketlist_item.py
@@ -1,4 +1,19 @@
 # A bucketlist item and all it's attributes
 
+
 class BucketListItem:
-    pass
+    ''' Tis is the bucketlist item object '''
+
+    def __init__(self, name, category, description=None, status=None, bucketlist=None):
+        ''' Constructing the bucketlist item while handling possible exceptions '''
+        # Ensure item belongs to a bucketlist
+        if bucketlist is None:
+            raise Exception("BucketList item must belong to a bucketlist")
+        self.name = name
+        self.category = category
+        self.description = description
+        self.bucketlist_id = bucketlist
+        if status is None:
+            self.status = "in_progress"
+        else:
+            self.status = status
