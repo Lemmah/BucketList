21573fde27165210a20dfa9bdd9253491f250e86
Lemmah <lemayiannakolah@gmail.com>
2017-07-20 00:07:27 +0300

    [feature] construct bucketlist controller
    Constructor for bucketlist controller.

--- app/bucketlist_controller.py
+++ app/bucketlist_controller.py
@@ -1,5 +1,12 @@
 # This is the super class, Controlling all the operations on the bucketlist and bucketlist items
 
+from app.bucketlist import BucketList
+
+
 class BucketListController:
     ''' Perfoming CRUD operations on bucket lists '''
-    pass
+
+    def __init__(self, user):
+        ''' Construct the bucketlist controller '''
+        self.user = user
+        self.available_bucketlists = []
