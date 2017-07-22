7aa11e1afcfe2b630e2fda50e5bbe8016284fd96
Lemmah <lemayiannakolah@gmail.com>
2017-07-23 00:37:45 +0300

    [fix] Subclassing unittest for assertions, more tests
    More tests on inheritance of the user class

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -2,6 +2,7 @@
 
 import unittest
 from app.user.user import User
+from app.bucketlist.bucketlist_controller import BucketListController
 
 
 class TestUser(unittest.TestCase):
@@ -19,16 +20,19 @@ class TestUser(unittest.TestCase):
   # 1. Test structure and user instance creation
   def test_user_inherits_bucketlistcontroler(self):
     ''' Test that user inherits bucketlist controller '''
-    self.assertEqual(isinstance(self.user_instance, BucketListController.User), True)
+    # Assert that it's a user instance
+    self.assertEqual(isinstance(self.user_instance, User), True)
+    # Assert that it's also a bucketlist instance
+    self.assertEqual(isinstance(self.user_instance, BucketListController), True)
 
   def test_user_has_all_properties(self):
     ''' Ensure user has name, email, password '''
-    assertEqual(self.user_instance.name, self.user_details[2])
-    assertEqual(self.user_instance.email, self.user_details[0])
-    assertEqual(self.user_instance.password, self.user_details[1])
+    self.assertEqual(self.user_instance.name, self.user_details[2])
+    self.assertEqual(self.user_instance.email, self.user_details[0])
+    self.assertEqual(self.user_instance.password, self.user_details[1])
 
   def test_user_representation(self):
     ''' Assert that __repr__ function returns a useful object representation '''
-    self.assertEqual(dict(self.user_instance), {'jnlemayian@gmail.com' : ['Password', 'James Lemayian']})
+    self.assertEqual(str(self.user_instance), "James Lemayian")
 
   ## The user is just an extension of the bucketlist controller
