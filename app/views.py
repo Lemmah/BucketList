8fead7dad7f59216609d9d4dd94625c061f46884
Lemmah <lemayiannakolah@gmail.com>
2017-07-22 19:15:13 +0300

    [chore] Recieve Form Data using flask request
    Recieve form data using flask request. This simply renders the data to confirm receipt.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask import render_template
 
 from app import app
@@ -13,4 +14,23 @@ def dashboard():
 @app.route('/logout')
 def logout():
   return render_template("index.html")
+
+@app.route('/login', methods=['POST'])
+def login():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    password = request.form.get('password')
+    return "{} - {}".format(email, password)
+  else:
+    return "Something went wrong with the post method."
+
+@app.route('/register', methods=['POST'])
+def register():
+  if request.method == 'POST':
+    email = request.form.get('email')
+    password = request.form.get('password')
+    name = request.form.get('username')
+    return "{} - {} - {}".format(name, email, password)
+  else:
+    return "Something went wrong with the post method."
 
