4e8d99774247867a54e7a0b16f742d16134a975b
Lemmah <lemayiannakolah@gmail.com>
2017-07-23 07:27:42 +0300

    [new] Model the User Object
    The User inherits from the BucketListController

---
+++ app/user/user.py
@@ -0,0 +1,11 @@
+# Modelling a user
+# A user is a bucketlist controller so,
+
+from app.bucketlist.bucketlist_controller import BucketListController
+
+class User(BucketListController):
+  ''' A user who is also a bucketlist controller '''
+  def __init__(self, email, password, name):
+    ''' Construct an instance of a user '''
+    pass
+
