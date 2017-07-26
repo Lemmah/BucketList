870f96ba0cbd521dbbf7a4cb466a29f1b50fe9f5
Lemmah <lemayiannakolah@gmail.com>
2017-07-26 18:42:50 +0300

    [chore] Generate Unique Secret Key

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # configurations for my app
 
+import uuid
+
 # Enable Flask's debugging features. Should be False in production
 DEBUG = True
 SESSION_TYPE = 'filesystem'
-SECRET_KEY = 'super secret key'
+SECRET_KEY = str(uuid.uuid4())
